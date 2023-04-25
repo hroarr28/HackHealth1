36825f217b23d7ffe4bb718d74c528a4e9b1a02c
hroarr28 <thcuk1@hotmail.co.uk>
2023-04-25 14:19:51 +0100

    Layout for Daily-Progress started

---
+++ hackhealth/src/components/Main-Page/Main-Page.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import DailyProgress from './Daily-Progress/Daily-Progress';
+// make a main page component
+
+function MainPage() {
+    return (
+        <div className="Main-Page">
+        <DailyProgress />
+        </div>
+    );
+}
+    
+export default MainPage;
