e59275adb1b3d498089ad7d66d42fd0a2f6d2bc5
Anjum Iqbal <anjum.r.iqbal@gmail.com>
2023-04-25 16:43:25 +0100

    added get username functionality

--- hackhealth/src/components/App/index.js
+++ hackhealth/src/components/App/index.js
@@ -1,15 +1,32 @@
-
 import DayButtons from "../Main-Page/Left-Side-Nav-Bar/Left-Side-Nav-Bar";
 import "./App.css";
-import MainPage from "../Main-Page/Main-Page"
+import MainPage from "../Main-Page/Main-Page";
 import LoginPage from "../Login-Page/LoginPage";
+import { useState } from "react";
 function App() {
+  const [name, setName] = useState("");
+  const [user, setUser] = useState({ username: "" });
+
+  function getUserName(event) {
+    setName(event.target.value);
+    console.log(event.target.value);
+  }
+
+  function handleClick() {
+    // event.preventDefault()
+    setUser({ username: name });
+    console.log(user.username);
+  }
+
   return (
     <main className="App">
-      <LoginPage />
+      <LoginPage
+        getUserName={getUserName}
+        handleClick={handleClick}
+        user={user}
+      />
       <DayButtons />
-      <MainPage/>
-
+      <MainPage />
     </main>
   );
 }
--- hackhealth/src/components/Login-Page/Login-Form/Login-Form.js
+++ hackhealth/src/components/Login-Page/Login-Form/Login-Form.js
@@ -1,23 +1,30 @@
 import React from "react";
 import "./LoginFormStyles.css";
-import {useState} from "react";
 
-
-function LoginForm() {
-const [username, setUsername] = useState("");
-
+function LoginForm({ getUserName, handleClick, user }) {
+  console.log(getUserName);
   return (
     <div className="Login-Form">
       <header>
-        <img src="https://i.ibb.co/tsP0RjZ/Hack-9.png" alt="LogoImg"/>
+        <img src="https://i.ibb.co/tsP0RjZ/Hack-9.png" alt="LogoImg" />
       </header>
-        <form>
-          <h3>Profile image</h3>
-          <img src="https://i.ibb.co/LCrMT1R/Untitled-800-800px-300-300px.png" alt="AvatarImg" className="avatar-img"/>
-          <h3>Username</h3>
-          <input type="text" placeholder="Username" name="username" required/>
-          <button>LOGIN</button>
-        </form>
+      <form>
+        <h3>Profile image</h3>
+        <img
+          src="https://i.ibb.co/LCrMT1R/Untitled-800-800px-300-300px.png"
+          alt="AvatarImg"
+          className="avatar-img"
+        />
+        <h3>Username</h3>
+        <input
+          type="text"
+          placeholder="Username"
+          name="username"
+          required
+          onChange={getUserName}
+        />
+        <button onClick={handleClick}>LOGIN</button>
+      </form>
     </div>
   );
 }
