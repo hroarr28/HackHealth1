905361d71f7e26593438f12f12dbd359637ba775
Anjum Iqbal <anjum.r.iqbal@gmail.com>
2023-04-26 14:07:36 +0100

    fixed tests

--- hackhealth/src/components/App/App.test.js
+++ hackhealth/src/components/App/App.test.js
@@ -1,8 +1,11 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./index";
+import { BrowserRouter as Router } from "react-router-dom";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test("renders learn react link", () => {
+  render(
+    <Router>
+      <App />
+    </Router>
+  );
 });
--- hackhealth/src/components/Login-Page/LoginPage.js
+++ hackhealth/src/components/Login-Page/LoginPage.js
@@ -3,7 +3,7 @@ import "./LoginPageStyles.css";
 import LeftImage from "./Image/Image";
 import LoginForm from "./Login-Form/Login-Form";
 
-function LoginPage({ getUserName, user, handleClick, navigate }) {
+function LoginPage({ getUserName, user, handleClick }) {
   return (
     <div className="login-page">
       <LoginForm
