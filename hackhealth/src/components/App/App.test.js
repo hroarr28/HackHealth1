136dc9b4b2b3e726de5ac4f7d8c35ce865cc000c
ashbsmith <aislingbsmith@gmail.com>
2023-04-26 15:30:44 +0100

    me and anjum being amazing

--- hackhealth/src/components/App/App.test.js
+++ hackhealth/src/components/App/App.test.js
@@ -1,11 +1,30 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import App from "./index";
 import { BrowserRouter as Router } from "react-router-dom";
 
-test("renders learn react link", () => {
-  render(
-    <Router>
-      <App />
-    </Router>
-  );
+// test("renders learn react link", () => {
+//   render(
+//     <Router>
+//       <App />
+//     </Router>
+//   );
+// });
+
+describe('Login Button', () => {
+  it('should go to next page on when logging in', () => {
+render(
+      <Router>
+        <App />
+      </Router>
+    );
+
+    const input = screen.getByRole('textbox');
+    const loginButton = screen.getByRole('button', {name: 'LOGIN'});
+
+    fireEvent.change(input, { target: { value: 'Ash' } });
+    fireEvent.click(loginButton);
+
+    // The text "Welcome" should be present on the page after successful login
+    expect(screen.getByRole('heading', {name: 'Welcome Back'})).toBeInTheDocument();
+  });
 });
