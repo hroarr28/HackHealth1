81e91e95eddd67eb214722eedfa2b7442f13bf4b
ashbsmith <aislingbsmith@gmail.com>
2023-04-27 10:20:36 +0100

    attempt at steps --> km conversion

--- hackhealth/src/components/Main-Page/Distance/Distance.js
+++ hackhealth/src/components/Main-Page/Distance/Distance.js
@@ -1,10 +1,15 @@
 import { useState } from "react";
 
-function Distance() {
+function Distance(props) {
+  const steps = props.steps;
+  const kmPerStep = 0.000762; // This is an approximate conversion rate from steps to kilometers
+
+  const distanceInKm = (steps * kmPerStep).toFixed(2);
+
   return (
     <div>
-      <h3>Distance</h3>
-      <p>1000 km</p>
+      <h3>Distance:</h3>
+      <p>{distanceInKm}</p>
     </div>
   );
 }
