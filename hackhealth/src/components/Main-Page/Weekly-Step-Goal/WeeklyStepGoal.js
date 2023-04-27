2cd994305130206c4d5d9d7959d698f79bffef2b
Anjum Iqbal <anjum.r.iqbal@gmail.com>
2023-04-27 11:31:54 +0100

    added on pressing enter functionality

--- hackhealth/src/components/Main-Page/Weekly-Step-Goal/WeeklyStepGoal.js
+++ hackhealth/src/components/Main-Page/Weekly-Step-Goal/WeeklyStepGoal.js
@@ -1,21 +1,35 @@
 import { useState } from "react";
-import "./WeeklyGoal.css"
+import "./WeeklyGoal.css";
 
 function WeeklyGoal(props) {
+  function handleKeyDown(event) {
+    if (event.key === "Enter") {
+      props.handleEnterClick();
+      event.target.value = "";
+    }
+  }
 
   return (
     <div className="weekly-goal">
       <div className="goal-form">
-      <h3>Set Your Weekly Step Goal</h3>
-      <img src="https://i.ibb.co/Chq4sQm/Untitled-300-300px.png" alt="steps-shoes"></img>
-      <input onChange={props.handleWeeklyEnterChange} type="text" />
-      <button onClick={props.handleEnterClick}>Submit</button>
+        <h3>Set Your Weekly Step Goal</h3>
+        <img
+          src="https://i.ibb.co/Chq4sQm/Untitled-300-300px.png"
+          alt="steps-shoes"
+        ></img>
+        <input
+          onChange={props.handleWeeklyEnterChange}
+          type="text"
+          onKeyDown={handleKeyDown}
+        />
+        <button onClick={props.handleEnterClick}>Submit</button>
       </div>
       <div className="goal-letter">
-      <h3>Weekly Step Goal</h3>
-      {props.showWeeklyStepGoal && <h2 className="step-goal">{props.weeklyStepGoal} Steeps</h2>}
+        <h3>Weekly Step Goal</h3>
+        {props.showWeeklyStepGoal && (
+          <h2 className="step-goal">{props.weeklyStepGoal} Steps</h2>
+        )}
       </div>
-    
     </div>
   );
 }
